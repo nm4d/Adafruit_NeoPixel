20c0b3b917792073541be78ca84c6a35c7efef01
David Wright <david.wright@bluewin.ch>
2016-05-19 16:52:28 +0200

    + Extended CVirtualLedStrip with its necessary methods to address the base class correctly. + Did not override the members that can be called from the base class Adafruit_NeoPixel. + Overrode the members that should not be called at all with a Serial.println() call. + Used CVirtualLedStrip in main.cpp instead of Adafruit_NeoPixel.

--- Adafruit_NeoPixel.h
+++ Adafruit_NeoPixel.h
@@ -129,7 +129,7 @@ class Adafruit_NeoPixel {
     virtual void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b);
     virtual void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 	virtual void setPixelColor(uint16_t n, uint32_t c);
-	virtual void setBrightness(uint8_t);
+	virtual void setBrightness(uint8_t b);
 	virtual void clear(void);
 	virtual void updateLength(uint16_t n);
 	virtual void updateType(neoPixelType t);
